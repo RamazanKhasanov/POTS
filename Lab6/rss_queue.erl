390cf4acf307a17d354d17a7adfbea7188476cff
Ramazan <xasanov_ramazan@bk.ru>
2024-04-01 11:50:09 +0300

    Lab6

---
+++ Lab6/rss_queue.erl
@@ -0,0 +1,107 @@
+-module(rss_queue).
+-include("logging.hrl").
+-compile(export_all).
+-define(TIMEOUT,10000).
+-include_lib("C:/Program Files/erl5.9.2/lib/xmerl-1.3.2/include/xmerl.hrl").
+
+-behaviour(gen_server).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
+         code_change/3]).
+
+-export([start/1]).
+
+-record(rssQ,{queue,subscribers}).
+
+start(Name) -> 
+  gen_server:start({local, Name}, ?MODULE, [], []).
+start(Name,Url)->
+  gen_server:start({local, Name}, ?MODULE, [Url], []).
+
+init([]) ->
+  process_flag(trap_exit,true),
+  {ok, #rssQ{queue=[],subscribers=sets:new()} };
+
+init([Url]) -> 
+  State = #rssQ{queue=[],subscribers=sets:new()},
+  process_flag(trap_exit,true),
+  rss_reader:start(Url,self()),
+  {ok, State }.
+
+handle_call(_Request={subscribe,QPid}, _From, State=#rssQ{queue=Q,subscribers=Subs}) ->
+  {Reply,NewState} = case sets:is_element(QPid,Subs) of
+    true -> {{error,already_subscribed},State};
+    false ->  erlang:monitor(process,QPid),
+    ?INFO("New subscriber ~p to ~p~n",[QPid,self()]),
+    [add_item(QPid,Item) || Item <- Q],
+    {ok,State#rssQ{subscribers=sets:add_element(QPid,Subs)}}
+  end,
+  {reply,Reply, NewState};
+
+handle_call(_Request={get_all}, _From, State=#rssQ{queue=Q}) -> 
+  {reply,Q,State};
+handle_call(_Request, _From, State) -> 
+  {reply,{error,{unknown_request,_Request}}, State}.
+
+handle_cast(_Msg={add_item,RSSItem=#xmlElement{name=item}}, State=#rssQ{queue=Q,subscribers=Subs}) -> 
+  NewQ = add_item_to_q(RSSItem,Q,Subs),
+  {noreply,State#rssQ{queue=NewQ}};
+
+handle_cast(_Msg={unsubscribe,QPid}, State=#rssQ{subscribers=Subs}) -> 
+  {noreply,State#rssQ{subscribers=sets:del_element(QPid,Subs)}};
+
+handle_cast(_Msg, State) -> 
+  ?WARN("Unknown msg {~p} to Q{~p}",[_Msg,State]),
+  {noreply, State}.
+
+handle_info(_Info={'DOWN',_,_,QPid,_Reason},State=#rssQ{subscribers=Subs})->
+  {noreply, State#rssQ{subscribers=sets:del_element(QPid,Subs)}};
+
+handle_info(_Info={'EXIT',FromPid,_Reason},State)->
+  ?ERROR("RSS Reader ~p died for ~p with reason ~n",[FromPid,self(),_Reason]),
+  {noreply, State};
+
+handle_info(_Info, State) -> 
+  {noreply, State}.
+
+terminate(_Reason, _State) -> ok.
+
+code_change(_OldVsn, State, _Extra) -> {ok, State}.
+
+subscribe(From,To)->
+  gen_server:call(To,{subscribe,From}).
+
+%% @doc добавление нового элемента в очередь  
+add_item_to_q(NewItem,Q,Subs)->
+  add_item_to_q(NewItem,[],Q,Subs).
+
+add_item_to_q(NewItem,L1,[],Subs)->
+  ?INFO("New item ~p ~n",[self()]),
+  broadcast(NewItem,Subs),
+  L1++[NewItem];
+
+add_item_to_q(NewItem,L1,L=[OldItem|Rest],Subs)->
+  case rss_parse:compare_feed_items(OldItem,NewItem) of
+    same -> 
+      L1++L ;
+    updated -> 
+      ?INFO("Updated item ~p ~n",[self()]),
+      broadcast(NewItem,Subs),
+      L1++Rest++[NewItem] ;
+    different -> 
+      add_item_to_q(NewItem,L1++[OldItem],Rest,Subs)
+  end.
+
+add_item(QPid,Item)->
+  ok = gen_server:cast(QPid , {add_item,Item} ),ok.
+
+add_feed(QPid,RSS2Feed)->
+ Items=rss_parse:get_feed_items(RSS2Feed),
+ [add_item(QPid,Item) || Item <- Items],
+ ?INFO("Added N=~p items from the feed to ~p ~n",[length(Items),QPid]),
+ ok. 
+
+get_all(QPid)->
+  gen_server:call(QPid,{get_all}).
+
+broadcast(Item,PidSet)->
+  [ add_item(Pid,Item) || Pid <- sets:to_list(PidSet) ]. 
---
+++ Lab6/rss_reader.erl
@@ -0,0 +1,28 @@
+-module(rss_reader).
+-include("logging.hrl").
+-compile(export_all).
+-define(RETRIEVE_INTERVAL,200000).
+
+start(Url,QPid)->
+  inets:start()
+  ,spawn(?MODULE,server,[Url,QPid]).
+
+server(Url,QPid)->
+  {ok,{Status={_,Code,_},_,Load}}=httpc:request(Url)
+
+  ,case Code of 
+    200 ->
+       {Feed,_} = xmerl_scan:string(Load)
+
+       ,case rss_parse:is_rss2_feed(Feed) of
+        ok -> 
+
+          rss_queue:add_feed(QPid,Feed)
+          ,receive
+            after ?RETRIEVE_INTERVAL -> 
+              server(Url,QPid)
+            end;
+        _ -> erlang:exit(not_rss2_feed)
+        end ; 
+    _ -> erlang:exit(Code)
+   end.
